ca680ba974c347ae1edbde16da6ef5d3f1f3ac02
ssong0320 <118571777+ssong0320@users.noreply.github.com>
2023-11-04 01:33:55 -0400

    Assignment 5, Created Create validator (ID, apr, account type, and initial cd balance)

--- src/main/java/CreateValidator.java
+++ src/main/java/CreateValidator.java
@@ -1,4 +1,59 @@
 public class CreateValidator {
     public boolean validate(String command) {
+        String[] tokens = command.split("\\s+");
+        String accountType = tokens[1];
+        String accountID = tokens[2];
+        String apr = tokens[3];
+
+        switch (accountType) {
+            case "cd":
+                if (tokens.length != 5) {
+                    return false;
+                }
+                try {
+                    double cdBalance = Double.parseDouble(tokens[4]);
+                    if (cdBalance <= 1000 || cdBalance >= 10000) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            case "checking":
+            case "savings":
+                if (tokens.length != 4) {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+        if (!isValidAccountID(accountID)) {
+            return false;
+        }
+
+        // Validate APR
+        if (!isValidAPR(apr)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidAPR(String apr) {
+        try {
+            double aprValue = Double.parseDouble(apr.replace("%", ""));
+            if (aprValue <= 10 || aprValue >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isValidAccountID(String accountID) {
+        return accountID.matches("\\d{8}");
     }
 }
